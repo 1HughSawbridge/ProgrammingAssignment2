d68bc026a05a326c2e12720692257536478a4e96
1HughSawbridge <hugh.sawbridge@gmail.com>
2015-01-20 17:13:09 +0000

    Hugh's attempt
    Help required

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#whilst I understand the concept of lexical scoping and the practical need to cache 
+#matrices to avoid inverting I don't really understand what is being asked here
+#or the need to keep setting and getting variables using functions (I'm sure it is
+#meant to be illustrative but still I don't get what it is trying to show)
+#any help/examples would be appreciated, this is what I have so far
 
+
+
+
+cacheSolve <- function( ) {
+       if(!is.null(vX)){
+         message("Fetchez la cache")
+         return(vX)
+       }else{
+       vX<-solve(X)    
+       }
+vX
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+makeCacheMatrix <- function(side=3) {
+  #creates an invertible square matrix of variable size  
+  
+  X<-matrix(runif(side,-side,+side),side,side)+diag(side)
+  
+  #inverts it 
+  vX<-solve(X)
+  vX
 }
